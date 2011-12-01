5696fa5d19fe7fe8e5b9418ee1155080b97e7bc7
mose <mose@mose.tw>
2011-12-01 22:21:46 +0800

    initial skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Agitracker::Application.config.secret_token = 'a5f3c3edcbd0aa520e49d9c9043786d17b4ec3984b6bafd815f97fd2ad8a2b2ebaf9dcaa25999cf2a4b12f0ee52493dc04686e3d55383bfaaba4b317554045a4'
