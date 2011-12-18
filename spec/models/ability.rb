efab1f924e9b0f86f113102c5946b94cd33fcd1f
mose <mose@mose.tw>
2011-12-18 21:41:00 +0800

    some small adjustments

--- spec/models/ability.rb
+++ spec/models/ability.rb
@@ -2,10 +2,11 @@ require 'spec_helper'
 require "cancan/matchers"
 
 describe Ability do
+
   describe "Guest user" do
     before :each do
-      user = Factory.build(:user)
-      @ability = Ability.new(user)
+      @user = Factory.build(:user)
+      @ability = Ability.new(@user)
     end
 
     it "can read pages" do
@@ -20,7 +21,22 @@ describe Ability do
     it "cannot list users" do
       @ability.should_not be_able_to(:read, User)
     end
+  end
 
+  describe "Editor user" do
+    before :each do
+      @user = Factory.build(:editor)
+      @ability = Ability.new(@user)
+    end
+
+    it "can update or destroy pages" do
+      @ability.should be_able_to(:update, Page)
+      @ability.should be_able_to(:destroy, Page)
+    end
+
+    it "cannot list users" do
+      @ability.should_not be_able_to(:read, User)
+    end
   end
 
 end
