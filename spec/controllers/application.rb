6e031264b412012e1077025a6464f8c9c326eb7c
mose <mose@mose.tw>
2011-12-18 21:41:08 +0800

    some small adjustments

---
+++ spec/controllers/application.rb
@@ -0,0 +1,15 @@
+require 'spec_helper'                                                                                     
+
+describe ApplicationController do
+
+  describe "access unauthorized page" do
+
+    it "fails miserably" do
+      get :show, { :controller => Admin::User, :action => :index }
+      flash[:error].should_not be_nil
+      response.should redirect_to root_url
+    end
+
+  end
+
+end
